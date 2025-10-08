0f93d5b18df1f7537832dba2839b631409afd161
liyaFPV <lbkmzc942@gmail.com>
2025-10-08 14:49:37 +0500

    Create disko-config.nix

---
+++ nixos/disko-config.nix
@@ -0,0 +1,44 @@
+{ lib, disko, ... }:
+{
+  disko.devices = {
+    disk = {
+      vda = {
+        type = "disk";
+        device = "/dev/vda"; # ЗАМЕНИТЕ на ваш диск!
+        content = {
+          type = "table";
+          format = "gpt";
+          partitions = [
+            # ESP раздел для загрузчика (не шифруется)
+            {
+              name = "ESP";
+              start = "0";
+              end = "512M";
+              bootable = true;
+              content = {
+                type = "filesystem";
+                format = "vfat";
+                mountpoint = "/boot";
+              };
+            },
+            # Основной раздел с LUKS шифрованием (весь остальной диск)
+            {
+              name = "luks";
+              start = "512M";
+              end = "100%";
+              content = {
+                type = "luks";
+                name = "crypted";
+                content = {
+                  type = "filesystem";
+                  format = "ext4";
+                  mountpoint = "/";
+                };
+              };
+            }
+          ];
+        };
+      };
+    };
+  };
+}
