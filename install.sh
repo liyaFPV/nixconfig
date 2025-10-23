3cf3a399d3838d297753b2843fd575925ff51ec2
liyaFPV <lbkmzc942@gmail.com>
2025-10-23 22:12:39 +0500

    исправлен install.sh

--- install.sh
+++ install.sh
@@ -5,3 +5,11 @@ cp -r hypr ~/.config/
 cp -r rofi ~/.config/
 
 sudo nixos-rebuild switch
+
+read -p "выполнить перезапуск? (y/n): " -n 1 -r
+echo
+if [[ $REPLY =~ ^[Yy]$ ]]; then
+    reboot
+else
+    echo "устоновка оконьчена"
+fi
