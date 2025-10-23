0e64ebd0a3560fa22ebdae09c4d687f702cda362
liyaFPV <lbkmzc942@gmail.com>
2025-10-23 22:01:05 +0500

    Убрать sudo из команд копирования конфигураций в install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 sudo chmod +x rofi/scripts/power.sh
 sudo cp -r nixos/* /etc/nixos
-sudo cp -r hypr ~/.config/
-sudo cp -r rofi ~/.config/
+cp -r hypr ~/.config/
+cp -r rofi ~/.config/
 
 sudo nixos-rebuild switch
